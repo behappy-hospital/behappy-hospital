e8fe68f8ffbbce5ef7e0dc28a01db82170a7daf0
xiaowu <943915349@qq.com>
2022-05-21 17:58:17 +0800

    update 1. behappy manager项目提交

---
+++ behappy-manager/src/main/java/org/xiaowu/behappy/manager/mapper/HospitalSetMapper.java
@@ -0,0 +1,10 @@
+package org.xiaowu.behappy.manager.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.xiaowu.behappy.manager.model.HospitalSet;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface HospitalSetMapper extends BaseMapper<HospitalSet> {
+
+}
---
+++ behappy-manager/src/main/java/org/xiaowu/behappy/manager/mapper/ScheduleMapper.java
@@ -0,0 +1,10 @@
+package org.xiaowu.behappy.manager.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.xiaowu.behappy.manager.model.Schedule;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ScheduleMapper extends BaseMapper<Schedule> {
+
+}
