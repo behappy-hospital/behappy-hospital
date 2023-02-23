c17d3afe4d9e3e7ddced429c911ac7cb83259254
xiaowu <943915349@qq.com>
2023-02-23 18:10:03 +0800

    push email

---
+++ pushEmail.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -eux
+
+CITY=$1
+LANGUAGE="zh-CN"
+UNIT=m
+UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
+
+curl \
+  -H "Accept-Language: $LANGUAGE" \
+  -H "User-Agent: $UA" \
+  -o result.html \
+  https://wttr.in/$CITY?format=4\&$UNIT
+
+echo '尚医通(改)项目已更新!' >> result.html
